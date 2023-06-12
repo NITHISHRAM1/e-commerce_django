82b99c94b0684e2e63928230d2deeb3f35112d9f
jjmnr.jayanthi26@gmail.com <jjmnr.jayanthi26@gmail.com>
2023-06-12 17:11:56 +0530

    first

--- eshop/eshop/settings.py
+++ eshop/eshop/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG') == 'True'
 # DEBUG = False
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['eshop-env.eba-r2wz32cv.ap-south-1.elasticbeanstalk.com','localhost']
 
 
 # Application definition
@@ -165,7 +165,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT='static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- eshop/order/models.py
+++ eshop/order/models.py
@@ -1,5 +1,3 @@
- 
-
 
 from django.db import models
 from django.contrib.auth.models import User
@@ -49,12 +47,14 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.id)
+
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
     order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True, related_name="orderitems")
     name = models.CharField(max_length=200, default="", blank=False)
     quantity = models.IntegerField(default=1)
     price = models.DecimalField(max_digits=7, decimal_places=2, blank=False)
+    image = models.CharField(max_length=500, default="", blank=False)
 
     def __str__(self):
         return str(self.name)
